88e7ced2345d73d7ac5861acb0734ea13fd8d157
VladanStar <vladancupric@gmail.com>
2023-01-13 09:59:47 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -46,35 +46,70 @@ console.log(dug);
 let prva = 1, druga = 2;
 console.log(prva + druga);
 
-// deklaracija i postavljanje vrednosti
-console.log('---');
-let osoba1 = 'Драгослав';
-let osoba2 = 'Драгослав';
-// u ovom trenutku promenljive osoba1 i osoba2 imaju istu vrednost
-console.log(osoba1);
-console.log(osoba2);
+// // deklaracija i postavljanje vrednosti
+// console.log('---');
+// let osoba1 = 'Драгослав';
+// let osoba2 = 'Драгослав';
+// // u ovom trenutku promenljive osoba1 i osoba2 imaju istu vrednost
+// console.log(osoba1);
+// console.log(osoba2);
 
-// promena vrednosti promenljive osoba1 ne utice na osoba2
-console.log('---');
-osoba1 = 'Павле';
-console.log(osoba1);
-console.log(osoba2);
+// // promena vrednosti promenljive osoba1 ne utice na osoba2
+// console.log('---');
+// osoba1 = 'Павле';
+// console.log(osoba1);
+// console.log(osoba2);
 
-// promena vrednosti promenljive osoba2 ne utice na osoba1
-console.log('---');
-osoba2 = 'Никола';
-console.log(osoba1);
-console.log(osoba2);
+// // promena vrednosti promenljive osoba2 ne utice na osoba1
+// console.log('---');
+// osoba2 = 'Никола';
+// console.log(osoba1);
+// console.log(osoba2);
 
-// postavljenje vrednosti
-console.log('---');
-osoba1 = 'Драгослав';
-osoba2 = osoba1;
-console.log(osoba1);
-console.log(osoba2);
+// // postavljenje vrednosti
+// console.log('---');
+// osoba1 = 'Драгослав';
+// osoba2 = osoba1;
+// console.log(osoba1);
+// console.log(osoba2);
 
-// promena vrednosti promenljive osoba1 ne utice na osoba2
-console.log('---');
-osoba1 = 'Павле';
-console.log(osoba1);
-console.log(osoba2);
+// // promena vrednosti promenljive osoba1 ne utice na osoba2
+// console.log('---');
+// osoba1 = 'Павле';
+// console.log(osoba1);
+// console.log(osoba2);
+
+
+// const osoba1 = "Никола";
+// console.log(osoba1);
+
+// let osoba2 = osoba1 + "!!!!";
+// console.log(osoba2);
+
+// nemoguce - pokusaj postavljanja 
+// vrednosti za konstantnu promenljivu
+//osoba1 = "Драгослав"
+
+// rad sa promenljivom deklarisanom
+// pomocu var
+{
+    var osoba3 = "Петар";
+    console.log(osoba3);
+}
+console.log(osoba3 + " " + osoba2)
+
+// rad sa promenljivom deklarisanom
+// pomocu let
+{
+    let osoba4 = "Јован"
+    console.log(osoba4)
+}
+// nemoguce - pokusaj pristupa
+// promenljvoj van njenog opsega
+// definisanosti
+console.log(оsoba4 + " " + osoba2)
+
+console.log(123 % 10);
+console.log(Infinity == Infinity)
+let vladan = "Vladan Cupric";
+console.log(vladan.replace("Vladan", "Milan"))
