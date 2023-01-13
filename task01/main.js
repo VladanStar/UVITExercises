a20a8e4ab6d061f9760fd2d77ea235dc8f28c3fd
VladanStar <vladancupric@gmail.com>
2023-01-13 09:25:07 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -23,3 +23,58 @@ console.log("1 === 'jedan' <=>", 1 === 'jedan');
 console.log("0 === 0 <=>", 0 === 0);
 console.log("false === false <=>", false === false);
 console.log("'jedan' === \"jedan\" <=>", 'jedan' === "jedan");
+
+// definicija promenljive
+let prom = 5 * 5;
+
+// prikaz vrednosti izraza koji sadrži promenljivu
+let broj = 10;
+console.log(broj * broj);
+
+// promenljiva ne mora imati svo vreme istu vrednost
+let raspolozenje = "Srećan \t :)";
+console.log(raspolozenje);
+raspolozenje = "Tužan \t :(";
+console.log(raspolozenje);
+
+// promenljiva se može uvećati i/ili umanjiti
+let dug = 200;
+dug = dug - 72;
+console.log(dug);
+
+// jednom naredbm se mogu definisati dve promenljive
+let prva = 1, druga = 2;
+console.log(prva + druga);
+
+// deklaracija i postavljanje vrednosti
+console.log('---');
+let osoba1 = 'Драгослав';
+let osoba2 = 'Драгослав';
+// u ovom trenutku promenljive osoba1 i osoba2 imaju istu vrednost
+console.log(osoba1);
+console.log(osoba2);
+
+// promena vrednosti promenljive osoba1 ne utice na osoba2
+console.log('---');
+osoba1 = 'Павле';
+console.log(osoba1);
+console.log(osoba2);
+
+// promena vrednosti promenljive osoba2 ne utice na osoba1
+console.log('---');
+osoba2 = 'Никола';
+console.log(osoba1);
+console.log(osoba2);
+
+// postavljenje vrednosti
+console.log('---');
+osoba1 = 'Драгослав';
+osoba2 = osoba1;
+console.log(osoba1);
+console.log(osoba2);
+
+// promena vrednosti promenljive osoba1 ne utice na osoba2
+console.log('---');
+osoba1 = 'Павле';
+console.log(osoba1);
+console.log(osoba2);
