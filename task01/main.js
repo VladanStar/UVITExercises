cd6ec4b0a92770966d84a97416b789b5e20bb479
VladanStar <vladancupric@gmail.com>
2023-01-13 09:00:26 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -3,3 +3,8 @@ for (let i = 0; i < 7; i++) {
     console.log(i);
 }
 console.log("крај");
+console.log(" Trla baba lan,\"\\n\" da joj prođe dan ");
+console.log("Trla \
+baba lan, \
+da joj prođe \
+dan ")
