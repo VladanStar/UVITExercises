29717d6e6fe3178cf3817974365047a1e866ae58
VladanStar <vladancupric@gmail.com>
2023-01-13 09:08:17 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -8,4 +8,18 @@ console.log("Trla \
 baba lan, \
 da joj prođe \
 dan ");
-console.log(" Trla baba lan,\n da joj prođe dan ")
+console.log(" Trla baba lan,\n da joj prođe dan ");
+
+
+console.log('Operator ==');
+console.log('0 == false <=>', 0 == false);
+console.log('42 == \'42\' <=>', 42 == '42');
+console.log('1 == "jedan" <=>', 1 == 'jedan');
+// Operator === zahteva da vrednosti imaju i isti tip i istu vrednost.
+console.log("Operator ===");
+console.log("0 === false <=>", 0 === false);
+console.log("42 === '42' <=>", 42 === '42');
+console.log("1 === 'jedan' <=>", 1 === 'jedan');
+console.log("0 === 0 <=>", 0 === 0);
+console.log("false === false <=>", false === false);
+console.log("'jedan' === \"jedan\" <=>", 'jedan' === "jedan");
