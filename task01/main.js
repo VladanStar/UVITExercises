92ab5d853b76da911bb07cf2232289b3d5a81adb
VladanStar <vladancupric@gmail.com>
2023-01-14 00:17:28 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -322,3 +322,68 @@ console.log(osoba3 + " " + osoba2)
 // nemoguce - pokusaj pristupa
 // promenljvoj van njenog opsega
 // definisanosti
+
+console.log('--- logicke vrednosti');
+console.log(undefined == true);
+console.log(undefined != true);
+console.log(undefined == false);
+console.log(undefined != false);
+
+console.log(`--- niska ''`);
+console.log(undefined > '');
+console.log(undefined < '');
+console.log(undefined == '');
+
+console.log(`--- niska '0'`);
+console.log(undefined > '0');
+console.log(undefined < '0');
+console.log(undefined == '0');
+
+console.log('--- broj 0');
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+
+console.log(`--- niska 'miki'`);
+console.log(undefined > 'miki');
+console.log(undefined < 'miki');
+console.log(undefined == 'miki');
+
+console.log('--- nedostajuce vrednosti');
+console.log(null == null);
+console.log(null == undefined);
+
+console.log('---')
+// zapis vrednosti undefined
+console.log(undefined);
+
+// poredjenja undefined sa drugim vrednostima
+console.log('--- broj 10');
+console.log(undefined > 10);
+console.log(undefined < 10);
+console.log(undefined == 10);
+
+console.log('---');
+// zapis vrednosti null
+console.log(null);
+
+// poredjenja null sa drugim vrednostima
+console.log('--- broj 10');
+console.log(null > 10);
+console.log(null < 10);
+console.log(null == 10);
+
+console.log('--- broj 0');
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+
+console.log(`--- niska 'miki'`);
+console.log(null > 'miki');
+console.log(null < 'miki');
+console.log(null == 'miki');
+
+console.log(`--- niska ''`);
+console.log(null > '');
+console.log(null < '');
+console.log(null == '');
