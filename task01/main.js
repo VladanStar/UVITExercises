19fdfbbab3f5522ccc0d2561c9a7bb88285f9f1e
VladanStar <vladancupric@gmail.com>
2023-01-14 18:41:11 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -438,3 +438,34 @@ function testirajOpsegDefinisanosti() {
 console.log(test);
 testirajOpsegDefinisanosti();
 console.log(test);
+
+
+console.log('---');
+// parametri i opsezi
+let x = 'van';
+const f1 = function () {
+    let x = 'unutar f1';
+    console.log(x);
+    // prikazaće: unutar f1
+};
+f1();
+console.log(x);
+// prikazaće: van
+
+console.log('---');
+const f2 = function () {
+    x = 'unutar f2';
+    console.log(x);
+    // prikazaće: unutar f2
+};
+f2();
+console.log(x);
+// prikazaće: unutar f2
+
+function stepen(osnova, izlozilac) {
+    if (izlozilac == 0)
+        return 1;
+    return osnova * stepen(osnova, izlozilac - 1);
+}
+
+console.log(stepen(3, 4))
