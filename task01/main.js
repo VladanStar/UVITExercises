5684d630ce01aab7f781ceee6c9ed55009e5b141
VladanStar <vladancupric@gmail.com>
2023-01-23 19:32:54 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -553,3 +553,16 @@ console.log("buducnost vraće:", buducnost());
 function buducnost() {
     return "Još uvek ne postoje leteći automobili";
 };
+test();
+
+var test = function () {
+    console.log("prikaz iz funkcijskog izraza");
+};
+
+test();
+
+function test() {
+    console.log("prikaz iz deklarisane funkcije");
+};
+
+test();
