03807545ea877328028158a2e1fec6955104e80a
VladanStar <vladancupric@gmail.com>
2023-01-29 16:22:43 +0100

    Add

--- task01/main.js
+++ task01/main.js
@@ -761,5 +761,18 @@ podsetiMe("priprema zadataka");
 hitnoMePodseti("odgovoriti na pisma");
 podsetiMe("kupovina knjige");
 
-while (podsetnik.length != 0) {
-    
+
+let sumaBrojeva = function () {
+    let ret = 0.0;
+    for (let i in arguments) {
+        let elem = Number(arguments[i]);
+        if (!isNaN(elem))
+            ret += elem;
+    }
+    return ret;
+}
+
+console.log(sumaBrojeva(1, 2, 3));
+console.log(sumaBrojeva(1, 2, 3, [1,2]));
+console.log(sumaBrojeva(1, 2, 3, [1]));
+console.log(sumaBrojeva("Miki", "1", 2, "100.5", 3));
