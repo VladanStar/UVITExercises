17b3a24074a7de5057762a3ce037617eeae14d45
VladanStar <vladancupric@gmail.com>
2023-01-29 19:37:16 +0100

    AAdd

--- task01/main.js
+++ task01/main.js
@@ -945,3 +945,14 @@ function filter(array, test) {
       .filter((person) => person.sex == "m")
       .filter(person.born > 1900 && person.born < 1925)
       );
+let arr =[1,2,3,4,5,6,7,8,9,]
+      function maxNumber(arr) {
+        let max = arr[0];
+        for (let i = 1; i < arr.length; i++) {
+          if (arr[i] > max) {
+            max = arr[i];
+          }
+        }
+        return max;
+      }
+      console.log(maxNumber(arr))
