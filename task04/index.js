c18f8866a3b5d8777203c3b671fa1a01f70aeb42
VladanStar <vladancupric@gmail.com>
2023-02-13 19:41:38 +0100

    Add

--- task04/index.js
+++ task04/index.js
@@ -1,18 +1,60 @@
-function prikaziNisku(niska, povratniPoziv) {
-    setTimeout(
-        () => {
-            console.log(niska);
-            povratniPoziv();
-        },
-        Math.floor(Math.random() * 50) + 1
-    );
+// function prikaziNisku(niska, povratniPoziv) {
+//     setTimeout(
+//         () => {
+//             console.log(niska);
+//             povratniPoziv();
+//         },
+//         Math.floor(Math.random() * 50) + 1
+//     );
+// }
+
+// function prikaziSve() {
+//     prikaziNisku("А", () => {});
+//     prikaziNisku("Б", () => {});
+//     prikaziNisku("В", () => prikaziNisku("Г", () => {}));
+//     prikaziNisku("Д", () => {});
+// }
+
+// prikaziSve();
+// function prikaziNisku(niska, povratniPoziv) {
+//   setTimeout(() => {
+//     console.log(niska);
+//     if (typeof povratniPoziv === "function") povratniPoziv();
+//   }, Math.floor(Math.random() * 50) + 1);
+// }
+
+// function prikaziSveRedom() {
+//   prikaziNisku("A", () => {
+//     prikaziNisku("Б", () => {
+//       prikaziNisku("В", () => {
+//         prikaziNisku("Г", () => {
+//           prikaziNisku("Д");
+//         });
+//       });
+//     });
+//   });
+// }
+
+// prikaziSveRedom();
+
+function prikaziNisku(niska) {
+    return new Promise((razresi, odbij) => {
+        setTimeout(
+            () => {
+                console.log(niska);
+                razresi();
+            },
+            Math.floor(Math.random() * 50) + 1
+        );
+    });
 }
 
 function prikaziSve() {
-    prikaziNisku("А", () => {});
-    prikaziNisku("Б", () => {});
-    prikaziNisku("В", () => prikaziNisku("Г", () => {}));
-    prikaziNisku("Д", () => {});
+    prikaziNisku("A");
+    prikaziNisku("Б");
+    prikaziNisku("В");
+    prikaziNisku("Г");
+    prikaziNisku("Д");
 }
 
 prikaziSve();
