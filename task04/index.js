cbffbb7b92febe372ec0324993a6be6ee5d8c877
VladanStar <vladancupric@gmail.com>
2023-02-13 21:15:08 +0100

    Add

--- task04/index.js
+++ task04/index.js
@@ -37,24 +37,82 @@
 
 // prikaziSveRedom();
 
-function prikaziNisku(niska) {
+// function prikaziNisku(niska) {
+//     return new Promise((razresi, odbij) => {
+//         setTimeout(
+//             () => {
+//                 console.log(niska);
+//                 razresi();
+//             },
+//             Math.floor(Math.random() * 50) + 1
+//         );
+//     });
+// }
+
+// function prikaziSve() {
+//     prikaziNisku("A");
+//     prikaziNisku("Б");
+//     prikaziNisku("В");
+//     prikaziNisku("Г");
+//     prikaziNisku("Д");
+// }
+
+// prikaziSve();
+
+// function asinhronaFunkcija() {
+//     return new Promise(function (resolve, reject) {
+//             //...kod za asinhronu operaciju...
+
+//             if (true) {
+//                resolve(result_value);
+//             } else {
+//                reject(error);
+//             }
+//         }
+//     );
+// }
+
+
+// function prikaziNisku(niska) {
+//     return new Promise((razresi, odbij) => {
+//         setTimeout(
+//             () => {
+//                 console.log(niska);
+//                 razresi();
+//             },
+//             Math.floor(Math.random() * 50) + 1
+//         );
+//     });
+// }
+
+// function prikaziTriRedom() {
+//     prikaziNisku("А")
+//         .then(() => {
+//             return prikaziNisku("Б");
+//         })
+//         .then(() => prikaziNisku("В"));
+//     prikaziNisku("Г");
+//     prikaziNisku("Д");
+// }
+
+// prikaziTriRedom();
+
+function dodajNisku(prethodna, tekuca) {
     return new Promise((razresi, odbij) => {
         setTimeout(
             () => {
-                console.log(niska);
-                razresi();
+                razresi(prethodna + ' ' + tekuca);
             },
             Math.floor(Math.random() * 50) + 1
-        );
-    });
+        )
+    })
 }
 
-function prikaziSve() {
-    prikaziNisku("A");
-    prikaziNisku("Б");
-    prikaziNisku("В");
-    prikaziNisku("Г");
-    prikaziNisku("Д");
+function dodajSveRedom() {
+    dodajNisku('', 'A')
+        .then(result => dodajNisku(result, 'Б'))
+        .then(result => dodajNisku(result, 'В'))
+        .then(result => console.log(result));
 }
 
-prikaziSve();
+dodajSveRedom();
